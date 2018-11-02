50208e8be6cb4b49c6ffa79bd9f58711d5b53ed7
yanh <891367701@qq.com>
2018-11-02 09:34:14 +0800

    Vue-cli配合Vue-Router和Animate.css的使用

--- vuerouterdemo/src/component/News.vue
+++ vuerouterdemo/src/component/News.vue
@@ -1,3 +1,26 @@
 <template>
-	<div>我是新闻</div>
+  <div>
+    <span>我是新闻</span>
+    <ul>
+      <li v-for="item in newsList">{{item}}</li>
+    </ul>
+    <div>
+    	<router-view></router-view>
+    </div>
+  </div>
 </template>
+<script type="text/javascript">
+export default {
+	name:"news",
+    data(){
+        return {
+        	newsList:['新闻1','新闻2','新闻3']
+        }
+    }
+}
+</script>
+<style scoped>
+li{
+	list-style: none;
+}
+</style>
--- vuerouterdemo/src/router.config.js
+++ vuerouterdemo/src/router.config.js
@@ -2,5 +2,14 @@
 * @Author: yanh
 * @Date:   2018-11-01 16:27:30
 * @Last Modified by:   yanh
-* @Last Modified time: 2018-11-01 16:27:30
+* @Last Modified time: 2018-11-02 09:31:11
 */
+import Home from './component/Home.vue'
+import News from './component/News.vue'
+ export default{
+ 	routes:[
+      {path:'/home',component:Home},
+      {path:'/news',component:News},
+      {path:'*',redirect:Home}
+ 	]
+ }
