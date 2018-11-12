2cbabad165dbf36f7194bd3695290af11ed16d1d
yanh <891367701@qq.com>
2018-11-12 22:50:17 +0800

    单一事件管理组件之间的通信

--- vuerouterdemo/src/router.config.js
+++ vuerouterdemo/src/router.config.js
@@ -2,5 +2,11 @@
 * @Author: yanh
 * @Date:   2018-11-01 16:27:30
 * @Last Modified by:   yanh
-* @Last Modified time: 2018-11-01 16:27:30
+* @Last Modified time: 2018-11-01 21:04:43
 */
+import Hoem from "./component/Home.vue"
+import Hoem from "./component/News.vue"
+export default{
+	
+}
+
